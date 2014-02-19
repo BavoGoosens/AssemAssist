2ca927d04bcb4afc55a1eba629b721b7aca1e3b7
Sander Geijsen <r0304675@pittem.cs.kotnet.kuleuven.be>
2014-02-19 11:31:43 +0100

    main file

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author r0304675
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sander");
+	}
+
+}
