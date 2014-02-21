426dcd6aaf5c2c31fa2c9e0323e43b019ea091dc
Bavo Goosens <bavo.goosens@gmail.com>
2014-02-21 11:59:13 +0100

    Added class Order

---
+++ src/Order.java
@@ -0,0 +1,44 @@
+/**
+ * A class representing a specific car order
+ * 
+ * @author Team 10
+ *
+ */
+public class Order {
+	
+	/**
+	 * A variable that contains the specific car being ordered
+	 */
+	private Car car;
+	
+	/**
+	 * The constructor for an order
+	 * 
+	 * @param car
+	 *        the car the user wants to order
+	 */
+	public Order( Car car){
+		setCar(car);
+	}
+	
+	/**
+	 * This method returns the ordered car
+	 * 
+	 * @return this.car
+	 */
+	public Car getCar() {
+		return car;
+	}
+
+	/**
+	 * This method sets the car being ordered 
+	 * 
+	 * @param car
+	 * 		  the car the user wants to order
+	 */
+	private void setCar(Car car) {
+		this.car = car;
+	}
+
+
+}
