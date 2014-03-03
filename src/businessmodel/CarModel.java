0a79d20c9c559c47de9d7e0b141db62da30f3b9e
Bavo Goosens <bavo.goosens@gmail.com>
2014-03-03 17:34:11 +0100

    beetje docu

--- src/businessmodel/CarModel.java
+++ src/businessmodel/CarModel.java
@@ -4,26 +4,78 @@ import java.util.ArrayList;
 
 import component.*;
 
+/**
+ * This car represents a car model. 
+ * A car model is an overview of lists of Components which the user can customize into a car.
+ * 
+ * @author Team 10
+ *
+ */
 public class CarModel {
 	
+	/**
+	 * The name of this car model.
+	 */
+	private String name;
+	
+	/**
+	 * A list with all the available body types.
+	 */
 	private ArrayList<Body> bodies;
 	
+	/**
+	 * A list of available colors for this model.
+	 */
 	private ArrayList<Color> colors;
 	
+	/**
+	 * A list of available engines for this car model.
+	 */
 	private ArrayList<Engine> engines;
 	
+	/**
+	 * A list of available gearboxes.
+	 */
 	private ArrayList<Gearbox> gearboxes;
 	
+	/**
+	 * A list of available seats.
+	 */
 	private ArrayList<Seats> seats;
 	
+	/**
+	 * A list of available airco installations.
+	 */
 	private ArrayList<Airco> aircos;
 	
+	/**
+	 * A list of available wheel types.
+	 */
 	private ArrayList<Wheels> wheels;
 	
+	/**
+	 * This method constructs the car model.
+	 * 
+	 * @param bodies The list of available car bodies.
+	 * 
+	 * @param colors The list of available car colors.
+	 * 
+	 * @param engines The list of available car engines.
+	 * 
+	 * @param gearboxes The list of available car gearboxes.
+	 * 
+	 * @param seats The list of available car seats.
+	 * 
+	 * @param aircos The list of available car aircos.
+	 * 
+	 * @param wheels The list of available car wheels.
+	 * 
+	 * @param name The name of this car model.
+	 */
 	public CarModel(ArrayList<Body> bodies, ArrayList<Color> colors,
 					ArrayList<Engine> engines, ArrayList<Gearbox> gearboxes,
 					ArrayList<Seats> seats, ArrayList<Airco> aircos,
-					ArrayList<Wheels> wheels) {
+					ArrayList<Wheels> wheels, String name) {
 		this.setBodies(bodies);
 		this.setColors(colors);
 		this.setEngines(engines);
@@ -31,8 +83,29 @@ public class CarModel {
 		this.setSeats(seats);
 		this.setAircos(aircos);
 		this.setWheels(wheels);
+		this.setName(name);
 	}
 	
+	/**
+	 * This method returns the name of this car model.
+	 * 
+	 * @return String
+	 * 		   The name of this car model.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * This method sets the name of this car model.
+	 * 
+	 * @param name
+	 * 		  The name of this car model.
+	 */
+	private void setName(String name) {
+		this.name = name;
+	}
+
 	public ArrayList<Body> getBodies() {
 		return this.bodies;
 	}
