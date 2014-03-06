24943c2b51d2361d887fcbd0376a2fc14a00c3ff
Bavo Goosens <bavo.goosens@gmail.com>
2014-03-06 22:11:46 +0100

    UC1 begint er op te trekken

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,13 +1,11 @@
 package control;
+import java.util.ArrayList;
+
 import ui.UserInterFace;
 import businessmodel.CarManufacturingCompany;
 import businessmodel.CarModel;
-import businessmodel.Catalogus;
-import businessmodel.GarageHolder;
-import businessmodel.Inventory;
-import businessmodel.Mechanic;
+import businessmodel.Order;
 import businessmodel.User;
-import businessmodel.UserManagement;
 
 /**
  * This class is the main controller instance. 
@@ -22,73 +20,37 @@ public class Controller {
 
 	private CarManufacturingCompany cmc; 
 
-	public Controller(CarManufacturingCompany cmc, UserInterFace ui){
-		this.cmc = cmc;
-		this.ui = ui;
+	public Controller(){
+		this.cmc = new CarManufacturingCompany(this);
+		this.ui = new UserInterFace(this);
 	}
 	
 	public void run(){
-		String[] res;
-		boolean login = false;
-		while (login == false) {
-			res = this.ui.loginPrompt();
-			login = cmc.login(res[0], res[1]);
-		}
-		User cu = cmc.getUser(res[0]);
-		if (cu instanceof GarageHolder){
-			this.orderNewCar(cu);
-		} else if( cu instanceof Mechanic){
-			this.performAssemblyTask(cu);
-		} else {
-			this.advanceAssemblyLine();
-		}
+		this.ui.login();
 	}
 
-
-	private void advanceAssemblyLine() {
-		// TODO Auto-generated method stub
-
+	public boolean authenticate(String username, String password) {
+		return this.cmc.login(username, password);
 	}
 
-	private void performAssemblyTask(User current_user) {
-		// TODO Auto-generated method stub
-
+	public User getUser(String username) {
+		return cmc.getUser(username);
 	}
 
-	private void orderNewCar(User current_user) {
-		while (true) {
-			String response = ui.displayOrderOverview(cmc.getCompletedOrders(current_user), cmc.getPendingOrders(current_user));
-			String response = ui.getInput();
-			if (response.equals("quit")){
-				// go back to login screen
-				break;
-			} else if (response.equals("order")){
-				String catalog = this.ca.getAvailableCarModels();
-				ui.displayString(catalog + "\n please select the car model you wish to order by entering the apropriate number: \n >");
-				response = ui.getInput();
-				if (response.equals("cancel")){
-					// display the overview and wait for input
-					this.orderOverview(current_user);
-					continue;
-				} else if (response.matches("\\d")){
-					int res = Integer.parseInt(response);
-					CarModel cm = this.ca.getCarModel(res);
-
-				}
-			} else if (response.equals("help")) {
-				this.ui.displayOrderHelp();
-			} else {
-				this.orderOverview(current_user);
-			}
-		}
-		this.run();
+	public boolean canPlaceOrder(User currentuser) {
+		return this.cmc.canPlaceOrder(currentuser);
 	}
 
-	private void orderOverview(User user){
-		String finished_orders = this.ps.getCompletedOrders(user);
-		String future_orders = this.ps.getPendingOrders(user);
-		ui.displayString("Your completed orders: \n \n "+ finished_orders);
-		ui.displayString("Your pending orders: \n \n" + future_orders);
+	public ArrayList<Order> getCompletedOrders(User currentuser) {
+		return this.cmc.getCompletedOrders(currentuser);
+	}
+
+	public ArrayList<Order> getPendingOrders(User currentuser) {
+		return this.cmc.getPendingOrders(currentuser);
+	}
+
+	public ArrayList<CarModel> getAvailableCarModels(User currentuser) {
+		return this.cmc.getAvailableCarModels(currentuser);		
 	}
 
 }
--- src/ui/UserInterFace.java
+++ src/ui/UserInterFace.java
@@ -2,8 +2,13 @@ package ui;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import component.Component;
+
 import control.Controller;
+import businessmodel.CarModel;
 import businessmodel.Order;
+import businessmodel.User;
 
 /**
  * This class provides a console user interface for the AssemAssist System.
@@ -19,7 +24,10 @@ public class UserInterFace {
 	private Scanner scan = new Scanner(System.in);
 
 	private Controller control;
-	
+
+	public UserInterFace(Controller control){
+		this.control = control;
+	}
 	/**
 	 * This method asks the current User for their login information.
 	 * 
@@ -27,21 +35,105 @@ public class UserInterFace {
 	 * 			An array of length 2 with the login information.
 	 * 
 	 */
-	public String[] loginPrompt(){
-		String[] result = new String[2];
-		System.out.println("Please enter your login information");
-		System.out.print("Username: ");
-		result[0] = this.scan.next();
-		System.out.print("Password: ");
-		result[1] = this.scan.next();
-		return result;
+	public void login(){
+		User currentuser;
+		while (true){
+			System.out.println("Please enter your login information");
+			System.out.print("Username: ");
+			String username = this.scan.next();
+			System.out.print("Password: ");
+			String password = this.scan.next();
+			if (this.control.authenticate(username,password)){
+				currentuser = this.control.getUser(username);
+				break;
+			} else {
+				this.badLogin();
+			}
+		}
+		this.order(currentuser);
+		this.performAssemblyTask(currentuser);
+		this.advance(currentuser);	
 	}
 
+	private void advance(User currentuser) {
+		// TODO Auto-generated method stub
+
+	}
+	private void performAssemblyTask(User currentuser) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void order(User currentuser){
+		while(true){
+			if (this.control.canPlaceOrder(currentuser)){
+				this.displayOrderOverview(currentuser);
+				this.displayOrderHelp();
+				String response = this.getInput();
+				if (response.equalsIgnoreCase("quit"))
+					break;
+				if (response.equalsIgnoreCase("order")){
+					ArrayList<CarModel> cml = this.control.getAvailableCarModels(currentuser);
+					CarModel cm = this.chooseCarModel(cml);
+					if (cm == null)
+						continue;
+					ArrayList<Component> carparts = this.chooseCar(cm);
+					Order order = new Order(currentuser,carparts, null);
+					this.control.placeOrder(order);
+				}
+				if (response.equalsIgnoreCase("help")){
+					this.displayOrderHelp();
+				}
+				if (response.equalsIgnoreCase("overview"))
+					continue;
+			}
+		}
+		this.login();
+	}
+
+	private ArrayList<Component> chooseCar(CarModel cm) {
+		ArrayList<ArrayList<Component>> possibleparts =  cm.getPosibilities();
+		ArrayList<Component> parts = new ArrayList<Component>();
+		for(ArrayList<Component> choices : possibleparts){
+			this.displayString("Enter the number of the" + choices.get(0).getClass() + "part you would like to order");
+			int count = 1;
+			for(Component comp : choices){
+				this.displayString(Integer.toString(count) + ") " + comp.toString());
+				count ++;
+			}
+			String response = this.getInput();
+			
+		}
+	}
+	
+	private CarModel chooseCarModel(ArrayList<CarModel> cml) {
+		this.displayString("Enter the number of the car model you wish to order or enter cancel to abort: \n");
+		int count = 1;
+		for(CarModel cm : cml){
+			this.displayString(Integer.toString(count) + ") " + cm.toString());
+			count ++;
+		}
+		String response = this.getInput();
+		// user wishes to cancel
+		if (response.equalsIgnoreCase("cancel"))
+			return null;
+		// user entered a number
+		if (response.matches("\\d*")){
+			int res = Integer.parseInt(response);
+			//user entered a valid number
+			if (res <= cml.size())
+				return cml.get(res - 1);	
+		}
+		return chooseCarModel(cml);	
+	}
+	
 	public void badLogin(){
 		System.out.println("We could not find you in the System \n \n");
 	}
 
-	public void displayOrderOverview(ArrayList<Order> completed, ArrayList<Order> pending){
+	public void displayOrderOverview(User currentuser){
+		ArrayList<Order> completed = this.control.getCompletedOrders(currentuser);
+		ArrayList<Order> pending = this.control.getPendingOrders(currentuser);
 		this.displayString("These are your completed orders: \n");
 		for (Order ord : completed){
 			this.displayString(ord.toString()+ "\n");
