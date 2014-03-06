6a3d156597e17dc1afbad1ef0e15148794cd92f8
Bavo Goosens <bavo.goosens@gmail.com>
2014-03-06 13:00:31 +0100

    dingen

---
+++ src/businessmodel/CarManufacturingCompany.java
@@ -0,0 +1,20 @@
+package businessmodel;
+
+public class CarManufacturingCompany {
+	
+	private UserManagement um;
+	
+	private Inventory inv;
+	
+	private OrderManagement om;
+	
+	private ProductionScheduler ps;
+	
+	public void login(String uname, String pwd) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
