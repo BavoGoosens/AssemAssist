7476b6b1b8582c1f9b3d732a05c641023c55b1a1
Michiel Vandendriessche <michielvdd@me.com>
2014-03-07 11:45:21 +0100

    Eerste versie van Assembly Line toegevoegd met doc. ProductionScheduler leeggemaakt

--- src/businessmodel/ProductionScheduler.java
+++ src/businessmodel/ProductionScheduler.java
@@ -1,15 +1,13 @@
 package businessmodel;
 
 public class ProductionScheduler {
-
-	public String getCompletedOrders(User current_user) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private CarManufacturingCompany cmc;
+	
+	public ProductionScheduler(CarManufacturingCompany cmc) {
+		this.cmc = cmc;
 	}
-
-	public String getPendingOrders(User current_user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
 
 }
