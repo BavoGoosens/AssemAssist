e1dea2d063fea286bd59cdd0307980a57f4521ca
SanderGn <sandergeijsen@hotmail.com>
2014-04-07 15:21:19 +0200

    Scheduler

--- src/businessmodel/scheduler/Scheduler.java
+++ src/businessmodel/scheduler/Scheduler.java
@@ -2,20 +2,30 @@ package businessmodel.scheduler;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-
 import org.joda.time.DateTime;
-
 import businessmodel.exceptions.IllegalSchedulingAlgorithmException;
 import businessmodel.order.Order;
 
 public class Scheduler {
 
+	/**
+	 * A list that holds all the shifts of this Scheduler. It holds at each moment 2 shifts of the current day.
+	 */
 	private ArrayList<Shift> shifts; 
 
+	/**
+	 * A list that holds all the orders of the car manufacturing company.
+	 */
 	private LinkedList<Order> orders;
 
+	/**
+	 * A variable that holds the current Scheduling Algorithm.
+	 */
 	private SchedulingAlgorithm algo;
 
+	/**
+	 * A Constructor for the class Scheduler. 
+	 */
 	public Scheduler(){
 		this.shifts = new ArrayList<Shift>();
 		this.orders = new LinkedList<Order>();
@@ -23,7 +33,15 @@ public class Scheduler {
 		generateShifts();	
 	}
 
-	public void changeAlgorithm(String algoname){
+
+	/**
+	 * A method to change the current algorithm that is used for scheduling.
+	 * @param	algoname
+	 * 			the new scheduling algorithm.
+	 * @throws 	IllegalSchedulingAlgorithmException
+	 * 			if the given algorithm is not implemented.
+	 */
+	public void changeAlgorithm(String algoname) throws IllegalSchedulingAlgorithmException{
 		if (algoname == null)
 			throw new NullPointerException("No scheduling algorithm supplied");
 		else if (algoname.equalsIgnoreCase("fifo") || algoname.equalsIgnoreCase("first in first out") )
@@ -34,6 +52,11 @@ public class Scheduler {
 			throw new IllegalSchedulingAlgorithmException("The scheduling algorithm was not recognised");
 	}
 
+	/**
+	 * A method to add a new Order. The Order will be added to the list of orders and scheduled into the system.
+	 * @param	order
+	 * 			the new order that needs to be scheduled.
+	 */
 	protected void addOrder(Order order){
 		if(order == null) 
 			throw new IllegalArgumentException();
@@ -41,21 +64,41 @@ public class Scheduler {
 		this.reschedule(order);
 	}
 
-	protected void schedule(){
+	/**
+	 * A method that schedules a new day. The shifts are emptied.
+	 */
+	protected void scheduleNewDay(){
+		for(Shift sh: this.getShifts())
+			sh.terminate();
+		this.generateShifts();
 		for (Order order: this.getOrders()){
 			reschedule(order);
 		}
 	}
 
+	/**
+	 * A method to get the shifts of the current day.
+	 * @return	this.shifts
+	 */
 	protected ArrayList<Shift> getShifts() {
 		return shifts;
 	}
 
+	/**
+	 * A method to schedule an individual Order into the schedule. The estimated completion time is updated.
+	 * @param	order
+	 * 			the order that needs to be scheduled.
+	 */
 	private void reschedule(Order order){
-		algo.schedule(order);
+		this.getAlgo().schedule(order);
 		this.updateTimeofOrder(order);	
 	}
 
+	/**
+	 * A method to update the estimated completion time of an order.
+	 * @param	order
+	 * 			the order for which the completion date needs to be updated.
+	 */
 	private void updateTimeofOrder(Order order) {
 		
 		DateTime date = this.getPrevious(order).getEstimateDate();
@@ -72,6 +115,9 @@ public class Scheduler {
 		}
 	}
 
+	/**
+	 * A method to generate new shifts for the current day.
+	 */
 	private void generateShifts(){
 		Shift currrentshift = new FreeShift(8);
 		Shift endshift = new EndShift(8);
@@ -79,15 +125,29 @@ public class Scheduler {
 		this.getShifts().add(endshift);
 	}
 
+	/**
+	 * a method to get the orders of the car manufacturing company.
+	 * @return	this.orders
+	 */
 	private LinkedList<Order> getOrders() {
 		return orders;
 	}
 
+	/**
+	 * A method to get the current scheduling algorithm of this scheduler.
+	 * @return	this.algo
+	 */
 	private SchedulingAlgorithm getAlgo() {
-		return algo;
+		return this.algo;
 	}
 
 	// TODO defensief maken IllegalArgumentException denk ik ??
+	/**
+	 * A method to get the previous order that has been scheduled.
+	 * @param 	order
+	 * 			the current order that needs to be scheduled.
+	 * @return	the previous order that has been scheduled.
+	 */
 	private Order getPrevious(Order order){
 		int index = this.getOrders().indexOf(order);
 		if(index-1 < 0)
