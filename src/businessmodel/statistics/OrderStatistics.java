6dba531e87e9730c75decd76b86a4d88947d5918
Bavo Goosens <bavo.goosens@gmail.com>
2014-04-11 14:32:59 +0200

    begin statistics en aanmaak util

---
+++ src/businessmodel/statistics/OrderStatistics.java
@@ -0,0 +1,20 @@
+package businessmodel.statistics;
+
+import businessmodel.observer.Observer;
+import businessmodel.observer.Subject;
+
+public class OrderStatistics implements Observer {
+
+	@Override
+	public void update(Subject s, Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Subject s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
