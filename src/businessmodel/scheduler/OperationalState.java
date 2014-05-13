41fcc44e80afe7fa790f90890dc43b37de4c27a4
BouweCeunen <bouwe.ceunen@gmail.com>
2014-05-13 19:26:29 +0200

    assembly line state

---
+++ src/businessmodel/scheduler/OperationalState.java
@@ -0,0 +1,33 @@
+package businessmodel.scheduler;
+
+public class OperationalState implements AssemblyLineState {
+
+	AssemblyLine assemblyLine;
+
+	/**
+	 * Constructor for operational state of the assembly line
+	 * @param assemblyLine
+	 */
+	public OperationalState(AssemblyLine assemblyLine){
+
+		if (assemblyLine == null) throw new IllegalStateException("Not a valid assembly line.");
+		this.assemblyLine = assemblyLine;
+
+	}
+
+	@Override
+	public void AssemblyLineIsBroken() {
+		this.assemblyLine.setState(this.assemblyLine.getBrokenState());
+	}
+
+	@Override
+	public void AssemblyLineIsOperational() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void AssemblyLineIsMaintenance() {
+		this.assemblyLine.setState(this.assemblyLine.getMaintenanceState());	
+	}
+
+}
