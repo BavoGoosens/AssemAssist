e8017c17777750a689e504719b5f9323320a0bed
SanderGn <sandergeijsen@hotmail.com>
2014-05-16 14:43:27 +0200

    henkie

--- src/businessmodel/MainScheduler.java
+++ src/businessmodel/MainScheduler.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 import businessmodel.assemblyline.*;
+import businessmodel.assemblyline.AssemblyLine;
 import businessmodel.category.VehicleOption;
 import businessmodel.order.Order;
 
@@ -19,28 +20,6 @@ public class MainScheduler {
 	public MainScheduler(OrderManager ordermanager){
 		this.setOrderManager(ordermanager);
 		this.generateAssemblyLines();
-	}
-
-	protected AssemblyLine placeOrder(Order order){
-		ArrayList<AssemblyLine> possiblelines = new ArrayList<AssemblyLine>();
-		for(AssemblyLine assem : this.getAssemblyLines()){
-			if(assem.getAssemblyLineScheduler().canAddOrder())
-				possiblelines.add(assem);
-		}
-		if(possiblelines.size() == 0)
-			return null;
-		AssemblyLine fastestassem = possiblelines.get(0);
-		for(AssemblyLine assem2 : possiblelines){
-			if(assem2.getEstimatedCompletionTimeOfNewOrder(order).isBefore(fastestassem.getEstimatedCompletionTimeOfNewOrder(order)))
-				if(assem2.getAssemblyLineScheduler().canAddOrder())
-					fastestassem = assem2;
-		}
-		fastestassem.getAssemblyLineScheduler().addOrderToSchedule(order);
-		return fastestassem;
-	}
-
-	protected ArrayList<AssemblyLine> getAssemblyLines() {
-		return this.assemblylines;
 	}
 
 	public ArrayList<AssemblyLineScheduler> getAssemblyLineSchedulers() {
@@ -68,25 +47,54 @@ public class MainScheduler {
 		this.ordermanager.placeOrderInFront(order);
 	}
 
+	public OrderManager getOrderManager() {
+		return ordermanager;
+	}
+
+	protected AssemblyLine placeOrder(Order order){
+		ArrayList<AssemblyLine> possiblelines = new ArrayList<AssemblyLine>();
+		for(AssemblyLine assem : this.getAssemblyLines()){
+			if(assem.getAssemblyLineScheduler().canAddOrder())
+				possiblelines.add(assem);
+		}
+		if(possiblelines.size() == 0)
+			return null;
+		AssemblyLine fastestassem = possiblelines.get(0);
+		for(AssemblyLine assem2 : possiblelines){
+			if(assem2.getEstimatedCompletionTimeOfNewOrder(order).isBefore(fastestassem.getEstimatedCompletionTimeOfNewOrder(order)))
+				if(assem2.getAssemblyLineScheduler().canAddOrder())
+					fastestassem = assem2;
+		}
+		fastestassem.getAssemblyLineScheduler().addOrderToSchedule(order);
+		return fastestassem;
+	}
+
+	protected ArrayList<AssemblyLine> getAssemblyLines() {
+		return this.assemblylines;
+	}
+
+	protected void changeSystemWideAlgorithm(String algo, ArrayList<VehicleOption> options) {
+		this.systemWideAlgo = algo;
+		for (AssemblyLine assemblyLine: this.getAssemblyLines()) {
+			assemblyLine.getAssemblyLineScheduler().changeAlgorithm(algo, options);
+		}
+	}
+
 	private void generateAssemblyLines() {
 		ArrayList<AssemblyLine> assemblylines = new ArrayList<AssemblyLine>();
 		AssemblyLineAFactory factoryA = new AssemblyLineAFactory();
 		AssemblyLineBFactory factoryB = new AssemblyLineBFactory();
 		AssemblyLineCFactory factoryC = new AssemblyLineCFactory();
-
+	
 		AssemblyLine line1 = factoryA.createAssemblyLine();
 		AssemblyLine line2 = factoryB.createAssemblyLine();
 		AssemblyLine line3 = factoryC.createAssemblyLine();
-
+	
 		assemblylines.add(line1);
 		assemblylines.add(line2);
 		assemblylines.add(line3);
-
+	
 		this.setAssemblyLines(assemblylines);
-	}
-
-	public OrderManager getOrderManager() {
-		return ordermanager;
 	}
 
 	private void setOrderManager(OrderManager ordermanager) {
@@ -95,13 +103,6 @@ public class MainScheduler {
 
 	private void setAssemblyLines(ArrayList<AssemblyLine> assemblylines) {
 		this.assemblylines = assemblylines;
-	}
-
-	protected void changeSystemWideAlgorithm(String algo, ArrayList<VehicleOption> options) {
-		this.systemWideAlgo = algo;
-		for (AssemblyLine assemblyLine: this.getAssemblyLines()) {
-			assemblyLine.getAssemblyLineScheduler().changeAlgorithm(algo, options);
-		}
 	}
 
 	public String currentSystemWideAlgorithmDescription() {
--- src/businessmodel/assemblyline/BodyWorkPostFactory.java
+++ src/businessmodel/assemblyline/BodyWorkPostFactory.java
@@ -27,11 +27,11 @@ public class BodyWorkPostFactory extends WorkPostFactory {
 	@Override
 	protected HashMap<String, Integer> createStandardTimes() {
 		HashMap<String,Integer> times = new HashMap<String,Integer>();
-		times.put("Vehicle model A", 50);
-		times.put("Vehicle model B", 70);
-		times.put("Vehicle model C", 60);
-		times.put("Vehicle model X", 90);
-		times.put("Vehicle model Y", 120);
+		times.put("Vehicle Model A", 50);
+		times.put("Vehicle Model B", 70);
+		times.put("Vehicle Model C", 60);
+		times.put("Vehicle Model X", 90);
+		times.put("Vehicle Model Y", 120);
 		return times;	
 	}
 }
