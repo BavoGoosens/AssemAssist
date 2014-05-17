8884372fd1d0ab2cd5caf5e565d50410c45bb5af
BavoGoosens <bavo.goosens@gmail.com>
2014-05-17 14:44:07 +0200

    mini foutje

--- src/ui/GarageHolderView.java
+++ src/ui/GarageHolderView.java
@@ -15,7 +15,7 @@ import businessmodel.exceptions.UnsatisfiedRestrictionException;
 import businessmodel.order.Order;
 import businessmodel.order.StandardVehicleOrder;
 import businessmodel.user.User;
-import businessmodel.util.OrderDateTimeComperator;
+import businessmodel.util.OrderDateTimeComparator;
 import control.StandardOrderController;
 import control.StandardOrderHandler;
 
@@ -220,8 +220,8 @@ public class GarageHolderView extends View{
 				this.pending_orders.add(pending.next());
 			while (completed.hasNext())
 				this.completed_orders.add(completed.next());	
-			Collections.sort(this.pending_orders, new OrderDateTimeComperator());
-			Collections.sort(this.completed_orders, new OrderDateTimeComperator());
+			Collections.sort(this.pending_orders, new OrderDateTimeComparator());
+			Collections.sort(this.completed_orders, new OrderDateTimeComparator());
 		} catch (NoClearanceException e) {
 			// NOP
 		}
