89971d0f2172bf5adeda960aff9ab687b609c4bd
SanderGn <sandergeijsen@hotmail.com>
2014-05-20 15:04:14 +0200

    henk

--- test/businessmodel/VehicleStatisticsTest.java
+++ test/businessmodel/VehicleStatisticsTest.java
@@ -149,7 +149,6 @@ public class VehicleStatisticsTest {
         }
         
         processOrders(om);
-
     }
     
     private void processOrders(OrderManager om) throws NoClearanceException {
