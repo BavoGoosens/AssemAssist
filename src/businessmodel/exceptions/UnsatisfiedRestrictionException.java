e80dc08650dac5fea1d3344996832bc6053fa840
BouweCeunen <bouwe.ceunen@gmail.com>
2014-05-21 00:34:12 +0200

    doc

--- src/businessmodel/exceptions/UnsatisfiedRestrictionException.java
+++ src/businessmodel/exceptions/UnsatisfiedRestrictionException.java
@@ -1,7 +1,12 @@
 package businessmodel.exceptions;
 
+/**
+ * UnsatisfiedRestriciontException
+ * 
+ * @author SWOP team 10
+ *
+ */
 public class UnsatisfiedRestrictionException extends Exception {
-
 	
 	private static final long serialVersionUID = 60666997797613313L;
 
