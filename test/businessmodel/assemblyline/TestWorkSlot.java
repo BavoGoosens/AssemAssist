a47a96beee4117e1e0e15832563a3314ed11224b
Michiel Vandendriessche <michielvdd@me.com>
2014-05-22 15:43:37 +0200

    shift test

--- test/businessmodel/assemblyline/TestWorkSlot.java
+++ test/businessmodel/assemblyline/TestWorkSlot.java
@@ -1,35 +1,46 @@
 package businessmodel.assemblyline;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import businessmodel.Catalog;
+import businessmodel.category.VehicleModel;
+import businessmodel.category.VehicleOption;
+import businessmodel.category.VehicleOptionCategory;
+import businessmodel.order.StandardVehicleOrder;
 import businessmodel.user.GarageHolder;
 import businessmodel.util.TestStandardVehicleOrder;
 
 public class TestWorkSlot {
 
 	private WorkSlot workslot;
-	private TestStandardVehicleOrder testorder;
+	private StandardVehicleOrder order;
 
 	@Before
 	public void setUp() throws Exception {
-		this.workslot = new WorkSlot();
-		testorder = new TestStandardVehicleOrder(new GarageHolder("","",""),"Car Model A");
-	}
-
-	@Test
-	// TODO Iets doen met de IllegalArgumentException
-	public void test() {
-
-		try {workslot.addOrder(null);}
-		catch (IllegalArgumentException e){
-			
+		Catalog catalog = new Catalog();
+		VehicleModel model = catalog.getAvailaleModelsClone().get(0);
+		ArrayList<VehicleOptionCategory> categories = catalog.getAllCategories();
+		ArrayList<VehicleOption> chosen = new ArrayList<VehicleOption>();
+		for (VehicleOptionCategory category: categories) {
+			if (model.getVehicleModelSpecification().getOptionsOfCategory(category).size() > 0) {
+				chosen.add(model.getVehicleModelSpecification().getOptionsOfCategory(category).get(0));
+			}
 		}
-		workslot.addOrder(testorder.getOrder());
-		assertEquals(workslot.getOrder(),testorder.getOrder());
+		this.order = new StandardVehicleOrder(new GarageHolder("Michiel", "Vandendriessche", "MichielVDD"), chosen, model);
+		this.workslot = new WorkSlot();
+	}
+	
+	@Test
+	public void test() {
+		this.workslot.addOrder(this.order);
+		assertEquals(this.order, workslot.getOrder());
 		assertTrue(this.workslot.isOccupied());
+		this.workslot.removeOrder();
+		assertFalse(this.workslot.isOccupied());
 	}
 }
